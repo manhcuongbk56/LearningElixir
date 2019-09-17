b94ad48eac6f4834a7b5e950f8b3ecb2a68d6cd6
manhcuong <manhcuong@example.com>
2019-09-17 21:28:45 +0700

    Add palindrome algorithm

--- Algorithm/palindrome.exs
+++ Algorithm/palindrome.exs
@@ -1,31 +1,43 @@
 defmodule Palindrome do
-  def longest_palindrome(s) do
-    codepoints = String.codepoints(s)
-    [head | tail] = codepoints
-    longest_palindrome([head], tail)
-  end
 
-  def longest_palindrome(left, right) do
-    [head_right | tail_right] = right
-    longest_now = even_palindrome([], left, right)
-    longest_after = longest_palindrome([head_right | left], tail_right)
-    if List.length(longest_now) > List.length(longest_after) do
-      longest_now
+  def longest_palindrome(s) when not is_binary(s), do: nil
+  def longest_palindrome(s) do
+    if String.length(s) < 2 do
+      s
     else
-      longest_after
+      [head | tail] = String.codepoints(s)
+      to_string(longest_palindrome([head], tail))
     end
   end
 
-  defp even_palindrome(result, left, right) when left == [] or right == [] do
-    result
-  end
-  defp even_palindrome(result, [head_left|left], [head_right|right]) do
-    IO.inspect binding()
-    if head_left == head_right do
-      tail = result ++ head_right
-      even_palindrome([head_left| tail], left, right)
+  defp longest_palindrome(_, []), do: []
+  defp longest_palindrome(left, right) do
+    [head_right | tail_right] = right
+    even = palindrome([], left, right)
+    odd = palindrome([head_right], left, tail_right)
+    longest_now = if length(even) > length(odd) do
+      even
     else
-      result
+      odd
+    end
+    #Slide from left to right to get longest palindrome of remain characters
+    longest_remain = longest_palindrome([head_right | left], tail_right)
+    if length(longest_now) > length(longest_remain) do
+      longest_now
+    else
+      longest_remain
+    end
+  end
+
+  defp palindrome(temp_result, left, right) when left == [] or right == [] do
+    temp_result
+  end
+  defp palindrome(temp_result, [head_left|left], [head_right|right]) do
+    if head_left == head_right do
+      tail = temp_result ++ [head_right]
+      palindrome([head_left| tail], left, right)
+    else
+      temp_result
     end
   end
 
---
+++ Algorithm/palindrome_test.exs
@@ -0,0 +1,29 @@
+ExUnit.start
+defmodule PalindromeTest do
+  use ExUnit.Case
+
+  test "return nil if input is not string" do
+    assert Palindrome.longest_palindrome(1223) == nil
+  end
+
+  test "return empty string" do
+    assert Palindrome.longest_palindrome("") == ""
+  end
+
+  test "return itself when length is even" do
+    assert Palindrome.longest_palindrome("abccba") == "abccba"
+  end
+
+  test "return itself when length is odd" do
+    assert Palindrome.longest_palindrome("abcdcba") == "abcdcba"
+  end
+
+  test "return right odd longest substring" do
+    assert Palindrome.longest_palindrome("babad") == "aba"
+  end
+
+
+  test "return right even longest substring" do
+    assert Palindrome.longest_palindrome("babbad") == "abba"
+  end
+end
