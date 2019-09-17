8b267688fa46d9f2fea6b53d37cd49a4aeda60d4
manhcuong <manhcuong@example.com>
2019-09-17 22:05:04 +0700

    Add doc, refactor function name

--- Algorithm/palindrome.exs
+++ Algorithm/palindrome.exs
@@ -1,32 +1,32 @@
 defmodule Palindrome do
   @doc """
-    Return longest palindrome substring of input
+    Returns longest palindrome substring of input
     ## Examples
       iex> Palindrome.longest_substring_palindrome("cabba")
       "abba"
   """
   def longest_substring_palindrome(s) when not is_binary(s), do: nil
   def longest_substring_palindrome(s) do
-    if String.length(s) < 2 do
-      s
-    else
+    if String.length(s) > 1 do
       [head | tail] = String.codepoints(s)
-      to_string(longest_palindrome([head], tail))
+      to_string(longest_substring_palindrome([head], tail))
+    else
+      s
     end
   end
 
-  defp longest_palindrome(_, []), do: []
-  defp longest_palindrome(left, right) do
+  defp longest_substring_palindrome(_, []), do: []
+  defp longest_substring_palindrome(left, right) do
     [head_right | tail_right] = right
-    even = palindrome([], left, right)
-    odd = palindrome([head_right], left, tail_right)
+    even = longest_palindrome_around_center([], left, right)
+    odd = longest_palindrome_around_center([head_right], left, tail_right)
     longest_now = if length(even) > length(odd) do
       even
     else
       odd
     end
-    #Slide from left to right to get longest palindrome of remain characters
-    longest_remain = longest_palindrome([head_right | left], tail_right)
+    #Travel center point from left to right to get longest palindrome of remain characters
+    longest_remain = longest_substring_palindrome([head_right | left], tail_right)
     if length(longest_now) > length(longest_remain) do
       longest_now
     else
@@ -34,13 +34,14 @@ defmodule Palindrome do
     end
   end
 
-  defp palindrome(temp_result, left, right) when left == [] or right == [] do
+  defp longest_palindrome_around_center(temp_result, left, right) when left == [] or right == [] do
     temp_result
   end
-  defp palindrome(temp_result, [head_left|left], [head_right|right]) do
+  defp longest_palindrome_around_center(temp_result, [head_left|left], [head_right|right]) do
     if head_left == head_right do
+      # Append to left and right side of temp_result and continue..
       tail = temp_result ++ [head_right]
-      palindrome([head_left| tail], left, right)
+      longest_palindrome_around_center([head_left| tail], left, right)
     else
       temp_result
     end
--- Algorithm/palindrome_test.exs
+++ Algorithm/palindrome_test.exs
@@ -1,29 +1,28 @@
 ExUnit.start
 defmodule PalindromeTest do
   use ExUnit.Case
-  doctest Palindrome
   test "return nil if input is not string" do
-    assert Palindrome.longest_palindrome(1223) == nil
+    assert Palindrome.longest_substring_palindrome(1223) == nil
   end
 
   test "return empty string" do
-    assert Palindrome.longest_palindrome("") == ""
+    assert Palindrome.longest_substring_palindrome("") == ""
   end
 
   test "return itself when length is even" do
-    assert Palindrome.longest_palindrome("abccba") == "abccba"
+    assert Palindrome.longest_substring_palindrome("abccba") == "abccba"
   end
 
   test "return itself when length is odd" do
-    assert Palindrome.longest_palindrome("abcdcba") == "abcdcba"
+    assert Palindrome.longest_substring_palindrome("abcdcba") == "abcdcba"
   end
 
   test "return right odd longest substring" do
-    assert Palindrome.longest_palindrome("babad") == "aba"
+    assert Palindrome.longest_substring_palindrome("babad") == "aba"
   end
 
 
   test "return right even longest substring" do
-    assert Palindrome.longest_palindrome("babbad") == "abba"
+    assert Palindrome.longest_substring_palindrome("babbad") == "abba"
   end
 end
